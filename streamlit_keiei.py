7b85e7f138cb7d77d54a8ad08c85b8aae928ea31
yoshipopo <109138096+yoshipopo@users.noreply.github.com>
2023-07-26 01:31:28 +0900

    Update streamlit_keiei.py

--- streamlit_keiei.py
+++ streamlit_keiei.py
@@ -218,7 +218,7 @@ def main():
     st.dataframe(df2)
     
     #result
-    fig = px.scatter(df2, x='収益率の分散', y='収益率の標準偏差',hover_name='投資比率',color='分類')
+    fig = px.scatter(df2, x='収益率', y='収益率の標準偏差',hover_name='投資比率',color='分類')
     fig.update_layout(height=500,width=800,
                       title='Result of MC : モンテカルロシミュレーション結果',
                       xaxis={'title': 'Variance of expected return : 期待収益率の分散'},
