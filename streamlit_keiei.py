a0902bb18c7b6a4e41bc09544171a849fe59afab
yoshipopo <109138096+yoshipopo@users.noreply.github.com>
2023-07-26 01:43:15 +0900

    Update streamlit_keiei.py

--- streamlit_keiei.py
+++ streamlit_keiei.py
@@ -202,16 +202,16 @@ def main():
 
     squares = [get_portfolio(x[i],np_mean,np_vcm) for i in range(x.shape[0])]
     df2 = pd.DataFrame(squares,columns=['投資比率','収益率', '収益率の分散'])
+
+    df2['収益率の標準偏差'] = np.sqrt(df2['収益率の分散'])
+    df2.drop(columns='収益率の分散')
+
     
     df2['分類']='PF{}資産で構成'.format(len(company_list_hyouji_datenashi))
     for i in range(x.shape[0]-n,x.shape[0]):
       df2.iat[i, 3] = company_list_hyouji_datenashi[i-x.shape[0]]
       #print(i,company_list_hyouji_datenashi[i-x.shape[0]])
-
-    st.dataframe(df2)
-    
-    df2['収益率の分散'] = np.sqrt(df2['収益率の分散'])
-    
+ 
     
     st.dataframe(df2)
     
